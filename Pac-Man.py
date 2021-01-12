39f81a20a0f8acdaf2e2256e5aba0b4794696a69
Fu1loN <75024824+Fu1loN@users.noreply.github.com>
2021-01-12 17:34:07 +0300

    Add files via upload

--- Pac-Man.py
+++ Pac-Man.py
@@ -1,10 +1,21 @@
 import pygame
 
 
+def get_pacman_cord(level):
+    for i in range(len(level)):
+        for j in range(len(level[i])):
+            if level[i][j] == '*':
+                return [j, i]
+
+
 class Board:
     def __init__(self, level):
         self.level = level
         self.cell_size = 30
+        self.width = len(self.level[0])
+        self.height = len(self.level)
+        self.left = 25
+        self.top = 25
 
     def render(self, screen):
         for i in range(len(self.level)):
@@ -32,13 +43,41 @@ class Board:
                     return i, j
 
 
+class PacMan(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__(all_sprites)
+        self.image = pygame.Surface((2 * 15 - 4, 2 * 15 - 4),
+                                    pygame.SRCALPHA, 32)
+        pygame.draw.circle(self.image, pygame.Color("yellow"),
+                           (13, 13), 13)
+        self.rect = pygame.Rect(*pos, 30, 30)
+        self.x_move = 0
+        self.y_move = 0
+
+    def update(self, *args):
+        if True:
+            self.rect = self.rect.move(self.x_move, self.y_move)
+
+    def change_way(self, ev):
+        if ev.scancode == 80:
+            self.x_move, self.y_move = -1, 0
+        if ev.scancode == 82:
+            self.x_move, self.y_move = 0, -1
+        if ev.scancode == 79:
+            self.x_move, self.y_move = 1, 0
+        if ev.scancode == 81:
+            self.x_move, self.y_move = 0, 1
+
+
 if __name__ == '__main__':
     pygame.init()
     menu_text = ['Pac-Man', '1 level']
     font = pygame.font.Font(None, 30)
     text_coord = 100
+
     width, height = 500, 500
     size = width, height
+
     screen_menu = pygame.display.set_mode(size)
     string_rendered = font.render(menu_text[0], 5, pygame.Color('white'))
     intro_rect = string_rendered.get_rect()
@@ -47,6 +86,7 @@ if __name__ == '__main__':
     intro_rect.x = 50
     text_coord += intro_rect.height
     screen_menu.blit(string_rendered, intro_rect)
+    all_sprites = pygame.sprite.Group()
     for line in menu_text[1:]:
         string_rendered = font.render(line, 1, pygame.Color('white'))
         intro_rect = string_rendered.get_rect()
@@ -56,6 +96,7 @@ if __name__ == '__main__':
         text_coord += intro_rect.height
         screen_menu.blit(string_rendered, intro_rect)
     running = True
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -77,14 +118,25 @@ if __name__ == '__main__':
         size = width, height
         screen_play = pygame.display.set_mode(size)
         running = True
+        all_sprites = pygame.sprite.Group()
+        clock = pygame.time.Clock()
+        pacman = PacMan(map(lambda x: board.left + board.cell_size * x, get_pacman_cord(level)))
+
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     print(board.find_cell(event.pos))
+                if event.type == pygame.KEYDOWN:
+                    pacman.change_way(event)
+
+            screen_play.fill((0, 0, 0))
+            pacman.update()
             board.render(screen_play)
+            all_sprites.draw(screen_play)
             pygame.display.flip()
+            clock.tick(30)
         pygame.quit()
     except FileNotFoundError:
         print('Файл с уровнем не найден')
